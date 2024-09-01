a0cd63b8a2ddd2019d66a91f56a34cd45a44dd6b
Eric_2139 <ericpowercosta2005@gmail.com>
2024-09-01 18:05:01 -0300

    adicionado mais coisa

---
+++ 02 - Estrutura de Dados/01 - LIstas/01_declarando_listas.py
@@ -0,0 +1,11 @@
+frutas = ['laranja', 'banana', 'uva']
+print(frutas)
+
+carro = ['Ferrari', 'F8', 420000, 2020, 2900, 'São Paulo', True]
+print(carro)
+
+letras = list('Python')
+print(letras)
+
+numeros = list(range(1,11))
+print(numeros)
